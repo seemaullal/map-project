4af502c85eb022710c0ffbb2e72955b8e39977c4
Brooke Seiter <bseiter@ucsd.edu>
2022-11-08 21:08:46 -0800

    Added env, gitignore, lat/lng columns

--- model.py
+++ model.py
@@ -5,44 +5,49 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-#Q: what is "user" in back_populates referring to? (ie table is users)
 #Q: how do you decide which attributes to list in the repr?
+#Q: 
 
 class User(db.Model):
-    """A user."""
+    """A User."""
 
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key=True)
-    fname = db.Column(db.String)
-    lname = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
-    username = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
-    phone_num = db.Column(db.Integer)
+    fname = db.Column(db.String, nullable=False)
+    lname = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    phone_num = db.Column(db.Integer, nullable=False)
 
     routes = db.relationship("Route", back_populates="user")
     spots = db.relationship("Spot", back_populates="user")
     reviews = db.relationship("Review", back_populates="user")
-    favorite_spots = db.relationship("Favorite_spots", back_populates="user")
+    favorite_spots = db.relationship("Favorite_spot", back_populates="user")
 
     def __repr__(self):
         return f'<User user_id={self.user_id} email={self.email}>'
 
 
 class Route(db.Model):
-    """A route."""
+    """A Route."""
 
     __tablename__ = "routes"
 
     route_id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key=True)
-    num_spots = db.Column(db.Integer)
-    total_miles = db.Column(db.Integer)
-    total_time = db.Column(db.datetime)
+    num_spots = db.Column(db.Integer, nullable=False)
+    total_miles = db.Column(db.Integer, nullable=False)
+    total_time = db.Column(db.DateTime, nullable=False)
+    start_lat = db.Column(db.Float, nullable=False)
+    start_lng = db.Column(db.Float, nullable=False)
+    end_lat = db.Column(db.Float, nullable=False)
+    end_lng = db.Column(db.Float, nullable=False)
+    miles_from_path = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
 
     reviews = db.relationship("Review", back_populates="route")
@@ -53,7 +58,7 @@ class Route(db.Model):
 
 
 class Spot_on_route(db.Model):
-    """A Spot on a route."""
+    """A Spot On a Route."""
 
     __tablename__ = "spot_on_route"
 
@@ -61,10 +66,10 @@ class Spot_on_route(db.Model):
                         autoincrement=True,
                         primary_key=True)
     route_id = db.Column(db.Integer, db.ForeignKey("routes.route_id"))
-    spot_id = db.Column(db.Integer, db.ForeignKey("spots.user_id"))
+    spot_id = db.Column(db.Integer, db.ForeignKey("spots.spot_id"))
 
-    route = db.relationship("Route", back_populates="spot_on_route")
-    spot= db.relationship("Spot_on_route", back_populates="spot_on_route")
+    routes = db.relationship("Route", back_populates="spot_on_route")
+    spots = db.relationship("Spot", back_populates="spot_on_route")
 
     def __repr__(self):
         return f'<Spot_on_route spot_on_route_id={self.spot_on_route_id}>'
@@ -78,14 +83,15 @@ class Spot(db.Model):
     spot_id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key=True)
-    miles_from_path = db.Column(db.Integer)
-    time_from_path = db.Column(db.datetime)
-    coordinates = db.Column(db.string)
+    miles_from_path = db.Column(db.Integer, nullable=False)
+    time_from_path = db.Column(db.DateTime, nullable=False)
+    spot_lat = db.Column(db.Float, nullable=False)
+    spot_lng= db.Column(db.Float, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
-    category_id = db.Column(db.Integer, db.ForeignKey("spot_category.spot_category_id"))
+    spot_category_id = db.Column(db.Integer, db.ForeignKey("spot_category.spot_category_id"))
 
-    user = db.relationship("Route", back_populates="spot")
-    spot_category= db.relationship("Spot_category", back_populates="spot")
+    users = db.relationship("User", back_populates="spot")
+    spot_categories = db.relationship("Spot_category", back_populates="spot")
 
     def __repr__(self):
         return f'<Spot_on_route spot_on_route_id={self.spot_on_route_id}>'
@@ -94,14 +100,14 @@ class Spot(db.Model):
 class Spot_category(db.Model):
     """A spot category."""
 
-    __tablename__ = "spot_category"
+    __tablename__ = "spot_categories"
 
     spot_category_id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key=True)
-    category_name = db.Column(db.String)
+    category_name = db.Column(db.String, nullable=False)
 
-    spot= db.relationship("Spot_category", back_populates="spot_category")
+    spots = db.relationship("Spot", back_populates="spot_category")
 
     def __repr__(self):
         return f'<Spot_category_id={self.spot_category_id}>'
@@ -115,15 +121,15 @@ class Review(db.Model):
     review_id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key=True)
-    rating = db.Column(db.Integer)
-    review_content = db.Column(db.Text)
+    rating = db.Column(db.Integer, nullable=False)
+    review_content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     route_id = db.Column(db.Integer, db.ForeignKey("routes.route_id"))
     spot_id = db.Column(db.Integer, db.ForeignKey("spots.spot_id"))
 
-    user = db.relationship("User", back_populates="review")
-    route = db.relationship("Route", back_populates="review")
-    spot = db.relationship("Spot", back_populates="review")
+    users = db.relationship("User", back_populates="review")
+    routes = db.relationship("Route", back_populates="review")
+    spots = db.relationship("Spot", back_populates="review")
 
     def __repr__(self):
         return f'<Review review_id={self.review_id}>'
@@ -132,7 +138,7 @@ class Review(db.Model):
 class Favorite_spot(db.Model):
     """A user's favorite spot."""
 
-    __tablename__ = "favorite_spot"
+    __tablename__ = "favorite_spots"
 
     favorite_spot_id = db.Column(db.Integer,
                         autoincrement=True,
@@ -140,9 +146,27 @@ class Favorite_spot(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     spot_id = db.Column(db.Integer, db.ForeignKey("spots.spot_id"))
 
-    user = db.relationship("User", back_populates="favorite_spot")
-    spot= db.relationship("Spot", back_populates="favorite_spot")
+    users = db.relationship("User", back_populates="favorite_spot")
+    spots = db.relationship("Spot", back_populates="favorite_spot")
 
     def __repr__(self):
         return f'<favorite_spot_id={self.favorite_spot_id}>'
+
+        
+
+def connect_to_db(flask_app, db_uri="postgresql:///roadtrip_database", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    connect_to_db(app)
 
