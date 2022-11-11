374cfda09e6ed1073e6cf32348493e805b7bdb76
Brooke Seiter <bseiter@ucsd.edu>
2022-11-10 20:54:58 -0800

    Created seed file, enables access to user data

---
+++ seed_database.py
@@ -0,0 +1,56 @@
+"""Script to seed database."""
+
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+
+import crud
+import model
+import server
+
+os.system("dropdb roadtrip_database")
+os.system("createdb roadtrip_database")
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+# Load user data from JSON file
+with open("data/users.json") as f:
+    user_data = json.loads(f.read())
+
+# Create users, store them in list so we can use them
+# to create fake spots/routes/reviews
+users_in_db = []
+for user in user_data:
+    fname, lname, email, username, password, phone_num = (
+        user["fname"],
+        user["lname"],
+        user["email"],
+        user["username"],
+        user["password"],
+        user["phone_num"],
+    )
+
+    db_user = crud.create_user(fname, lname, email, username, password, phone_num)
+    users_in_db.append(db_user)
+
+model.db.session.add_all(users_in_db)
+model.db.session.commit()
+
+# Create 10 users; each user will make 10 ratings
+# for n in range(10):
+#     email = f"user{n}@test.com"  # Voila! A unique email!
+#     password = "test"
+
+#     user = crud.create_user(email, password)
+#     model.db.session.add(user)
+
+#     for _ in range(10):
+#         random_movie = choice(movies_in_db)
+#         score = randint(1, 5)
+
+#         rating = crud.create_rating(user, random_movie, score)
+#         model.db.session.add(rating)
+
+# model.db.session.commit()
