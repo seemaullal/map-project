609bc18bc0d18c0c3e57ba9b14a0f36f2c23e5f7
Brooke Seiter <bseiter@ucsd.edu>
2022-11-17 10:50:53 -0800

    Added login and create account page inputs

--- front_end/src/CreateAccountPage.js
+++ front_end/src/CreateAccountPage.js
@@ -1,11 +1,22 @@
 export default function CreateAccountPage (props) {
-
-    const Something = "Say something"
-
     return ( 
         <div className="CreateAccountPage">
             <h2>Create An Account</h2>
-            <p>{ Something }</p>
+            <form className="CreateAccountForm">
+                <label>First name:</label>
+                <input type="text" required />
+                <label>Last name:</label>
+                <input type="text" required />
+                <label>Email:</label>
+                <input type="text" required />
+                <label>Username:</label>
+                <input type="text" required />
+                <label>Password:</label>
+                <input type="text" required />
+                <label>Phone Number:</label>
+                <input type="text" required />
+                <button>Submit</button>
+            </form>
         </div>
      );
 }
--- front_end/src/LoginPage.js
+++ front_end/src/LoginPage.js
@@ -2,6 +2,13 @@ const LoginPage = () => {
     return ( 
         <div className="LoginPage">
             <h2>Login</h2>
+            <form className="LoginForm">
+                <label>Username:</label>
+                <input type="text" required />
+                <label>Password:</label>
+                <input type="text" required />
+                <button>Sign In</button>
+            </form>
         </div>
      );
 }
